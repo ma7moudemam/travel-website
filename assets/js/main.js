9258d28105e057af5e42decbfc983962d1ff3294
m7moud emam <m7moud_emam@hotmail.com>
2021-08-21 00:15:45 +0200

    footer

--- assets/js/main.js
+++ assets/js/main.js
@@ -89,3 +89,32 @@ function finalVideo(){
 
 //ended , whe the video Ends 
 videoFile.addEventListener('ended',finalVideo)
+
+/* ================= Show Scroll Top ===========================*/
+function scrollUp(){
+    const scrollUp = document.getElementById('scroll-up');
+    // when the scroll is higher than 200 viewport height , add the show scroll class to the a tag with the scroll
+    if (this.scrollY>=200) scrollUp.classList.add('show-scroll');else scrollUp.classList.remove('show-scroll');
+}
+window.addEventListener('scroll',scrollUp);
+
+
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
