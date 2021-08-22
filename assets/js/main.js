bdb8f73c0393c2e5e2d5a358da360ea9cd22b7f8
m7moud emam <m7moud_emam@hotmail.com>
2021-08-22 03:07:02 +0200

    final

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,8 +22,6 @@ if (navClose){
     })
 }
 
-
-/* ====================== Remove MENU MOBILE ===============================*/
 /*==================== REMOVE MENU MOBILE ====================*/
 const navLink = document.querySelectorAll('.nav__link')
 
@@ -90,7 +88,7 @@ function finalVideo(){
 //ended , whe the video Ends 
 videoFile.addEventListener('ended',finalVideo)
 
-/* ================= Show Scroll Top ===========================*/
+/* ================= Show Scroll UP ===========================*/
 function scrollUp(){
     const scrollUp = document.getElementById('scroll-up');
     // when the scroll is higher than 200 viewport height , add the show scroll class to the a tag with the scroll
@@ -118,3 +116,58 @@ function scrollActive(){
     })
 }
 window.addEventListener('scroll', scrollActive)
+
+/* ===================== DARK LIGHT THEME =======================*/
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'ri-sun-line'
+
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'ri-moon-line' : 'ri-sun-line'
+    
+// We validate if the user previously chose a topic
+if (selectedTheme) {
+  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+  themeButton.classList[selectedIcon === 'ri-moon-line' ? 'add' : 'remove'](iconTheme)
+}
+
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+    // Add or remove the dark / icon theme
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+    // We save the theme and the current icon that the user chose
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
+
+/* ================= SCROLL REVEAL ANIMATION ===========================*/
+const sr = ScrollReveal({
+    distance:'60px',
+    duration: 2800 ,
+    // reset:true,
+
+})
+
+sr.reveal(`.home__data, .home__socail-link, .home__info , .discover__container,
+            .experience__data , .experience__overlay, .place__card,
+            .sponsor__content, .footer__rights , .footer__data`,{
+    origin:'top',
+    interval:100,
+
+})
+
+sr.reveal(`.about__data , .video__description, .subscribe__description`,{
+    origin:'left',
+})
+
+sr.reveal(`.about__img-overlay , .video__content, .subscribe__form`,{
+    origin:'right',
+    interval:100,
+})
